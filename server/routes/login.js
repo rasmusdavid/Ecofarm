30fb57ef4ac6523ac6b19c9beb142c3f2a67b3b6
rasmusdavid <rasmusdavidsson@hotmail.com>
2023-04-06 09:05:11 +0200

    session and login

---
+++ server/routes/login.js
@@ -0,0 +1,45 @@
+import { response, Router } from "express";
+import mongoose from "mongoose";
+
+const loginRouter = Router()
+
+loginRouter.post('/', async (request, response)=> {
+    const user = await mongoose.models.users.findOne({
+        email: request.body.email,
+        password: request.body.password
+    })
+
+    if(user){
+        response.status(201)
+        request.session.user = user
+        response.json('You logged in')
+    }
+    else{
+        response.status(401)
+        response.json('Wrong username or password')
+    }
+})
+
+loginRouter.get('/', async(request, response)=>{
+    if(request.session?.user){
+        const user = await mongoose.models.users.findOne({
+            email: request.session.user.email,
+            password: request.session.user.password
+        })
+        if(user){
+            response.json({
+                username: request.session.user.username,
+                email: request.session.user.email,
+                LoggedIn : true
+            })
+            return
+        }
+    } response.json({LoggedIn : false})
+})
+
+loginRouter.delete('/', (request, response)=>{
+    delete(request.session.user)
+    response.json('Logged out')
+})
+
+export default loginRouter
