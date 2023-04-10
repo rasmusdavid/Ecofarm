f6d9c8fa8516eafbe990ec7f4d8f3ee7b0f3f2af
rasmusdavid <rasmusdavidsson@hotmail.com>
2023-04-10 17:23:18 +0200

    cart button linked to cartpage

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -6,6 +6,7 @@ import AccountCard from './AccountCard';
 import { useState, useContext } from 'react';
 import GlobalContext from '../routing/Context';
 import MessageCard from './MessageCard';
+import UserCartPage from '../pages/UserCartPage';
 
 export default () => {
     const [content, setContent] = useState("Make your choice with the buttons.")
@@ -14,9 +15,10 @@ export default () => {
     return <Card style={{maxWidth: "600px"}}>
                 <Card.Header><h1>Welcome {auth.username}</h1></Card.Header>
                 <Card.Body style={{}}>
-                    <Button variant="success" style={{width:"100%", maxWidth:"150px", margin: "10px"}}>{auth.admin ? "Manage" : "Go Shop"}</Button>
-                    <Button variant="success" style={{width:"100%", maxWidth:"150px", margin: "10px"}} onClick={ ()=> setContent(<AccountCard />)}>Account</Button>
-                    <Button variant="success" style={{width:"100%", maxWidth:"150px", margin: "10px"}} onClick={ ()=> setContent(<MessageCard/>)}>Messages</Button>
+                    <Button variant="success" style={{width:"100%", maxWidth:"100px", margin: "10px"}}>{auth.admin ? "Manage" : "Go Shop"}</Button>
+                    <Button variant="success" style={{width:"100%", maxWidth:"100px", margin: "10px"}} onClick={ ()=> setContent(<AccountCard />)}>Account</Button>
+                    <Button variant="success" style={{width:"100%", maxWidth:"100px", margin: "10px"}} onClick={ ()=> setContent(<MessageCard/>)}>Messages</Button>
+                    <Button variant="success" style={{width:"100%", maxWidth:"100px", margin: "10px"}} onClick={ ()=> setContent(<UserCartPage/>)}>Cart</Button>
                     <LogoutButton/>
                 </Card.Body>
                 {content}
--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom"
-import GlobalContext from "../routing/Context";
-import { useContext } from 'react'
+import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,47 +7,15 @@ import Button from "react-bootstrap/esm/Button";
 
 
 export default () => {
-    const { auth } = useContext(GlobalContext)
-
-
-    if (auth.LoggedIn) {
-        return <Container fluid="md">
-            <Row className="p-3 mb-2 bg-success text-white" style={{ maxWidth: "600px" }}>
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <h1>Ecofarm</h1>
-                        </div>
-
-                        <div class="col">
-                            <Button className="btn btn-light text-success" style={{ width: "100%", maxWidth: "150px", margin: "10px" }} onClick={() => setContent(<MessageCard />)}>Cart</Button>
-                        </div>
-                    </div></div>
-
-            </Row>
-            <Row>
-                <Outlet />
-            </Row>
+    return <>
+        <Container fluid="md">
+                <Row className="p-3 mb-2 bg-success text-white" style={{ maxWidth: "600px" }}>
+                <h1>EcoFarm</h1>    
+                </Row>
+                <Row>
+                    <Outlet />
+                </Row>
         </Container>
-
-    } else {
-        return <Container fluid="md">
-            <Row className="p-3 mb-2 bg-success text-white" style={{ maxWidth: "600px" }}>
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <h1>Ecofarm</h1>
-                        </div>
-
-                    </div></div>
-
-            </Row>
-            <Row>
-                <Outlet />
-            </Row>
-        </Container>
-    }
-
-
+    </>
 
 }
