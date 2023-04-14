695ac64a74b762d9eefb02d045f35e9fbe6d3fb3
rasmusdavid <rasmusdavidsson@hotmail.com>
2023-04-14 17:51:04 +0200

    testar

--- server/routes/order.js
+++ server/routes/order.js
@@ -5,23 +5,24 @@ const orderRouter = Router();
 
 const orderScehema = new Schema({
   items: [new Schema({
-                      subcat: String,
-                      item: String,
-                      weight: Number,
-                      price: Number})],
+    subcat: String,
+    item: String,
+    weight: Number,
+    price: Number
+  })],
   email: String,
   sendid: String,
   total: Number,
-  verify: {type:Boolean, default:false}
+  verify: { type: Boolean, default: false }
 });
 
 mongoose.model("orders", orderScehema);
 
 orderRouter.get("/", async (request, response) => {
   //const order = await mongoose.models.orders.find();
-  if(request.session?.user?.email) {
-    const results = await mongoose.models.orders.find({ email: request.session.user.email})
-  response.json(results);
+  if (request.session?.user?.email) {
+    const results = await mongoose.models.orders.find({ email: request.session.user.email })
+    response.json(results);
   }
 });
 
